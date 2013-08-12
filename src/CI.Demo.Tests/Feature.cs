131915d1af0cc943f7bc370f3bad5fa42689b31b
David Muto <david.muto@gmail.com>
2013-08-12 15:02:16 -0400

    Initial commit

---
+++ src/CI.Demo.Tests/Feature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using Should.Core;
+using Should.Fluent;
+
+namespace CI.Demo.Tests
+{
+    public class Feature
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void ValidatesNameIsNotNull()
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                {
+                    new Demo.Feature(null);
+                });
+            }
+
+            [Fact]
+            public void ValidatesNameIsNotEmpty()
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                {
+                    new Demo.Feature("");
+                });
+            }
+
+            [Fact]
+            public void StoreTheNameInLowercase()
+            {
+                var subject = new Demo.Feature("My UPPERcase Feature");
+                subject.Name.Should().Equal("my uppercase feature");
+            }
+        }
+    }
+}
---
+++ src/CI.Demo/Feature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CI.Demo
+{
+    public class Feature
+    {
+        public string Name { get; private set; }
+
+        public Feature(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            this.Name = name.ToLower();
+        }
+    }
+}
